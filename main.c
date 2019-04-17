396627cf06c79c3e7d92bff22cc9c20687f4da14
Aleksander Brymora <aleksander.australia@gmail.com>
2019-04-17 20:06:23 +0000

    Reading and writing in external files.

--- main.c
+++ main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 
 int main () {
-    int a, b, c;
-    scanf("%d %d %d", &a, &b, &c);
-    printf("%d %d %d", a, b, c);   
+    FILE *input;
+    FILE *output;
+    int spaces = 0;
+    char cypher(char x);
+    
+    input = fopen("input.txt", "r");
+    output = fopen("output.txt", "w");
+    
+    while(!feof(input)) {
+        char charFromFile;
+        //Read Character
+        fscanf(input, "%c", &charFromFile);
+        
+        //Put a function for encryption here
+        charFromFile = cypher(charFromFile);
+        
+        //Print to the screen
+        fprintf(output,"%c", charFromFile);
+        
+
+    }
+    printf("%d\n", spaces);
+}
+
+char cypher(char x) {
+    return x;
 }
